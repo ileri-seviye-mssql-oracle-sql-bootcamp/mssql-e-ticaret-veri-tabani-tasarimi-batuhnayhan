6000e396f909a4561af068d5d982c3f2081d9b28
batuhnayhan <58702681+batuhnayhan@users.noreply.github.com>
2020-11-27 00:22:03 +0300

    Add files via upload

---
+++ E_TRADE_DATABASE/Views/View_1.sql
@@ -0,0 +1,2 @@
+CREATE VIEW [En_Pahalý_5_Sipariþin_Detaylarý] AS
+select TOP 5 * from ORDER_DETAILS ORDER BY TOTAL DESC
---
+++ E_TRADE_DATABASE/Views/View_2.sql
@@ -0,0 +1,2 @@
+CREATE VIEW [Ülke_Ýsimleri] AS
+select COUNTRY from COUNTRIES
